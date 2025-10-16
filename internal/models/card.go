02c7a7b4a3233af43b2ca52916b3226169fdbfcf
rookgm <misharook27@gmail.com>
2025-10-16 23:06:18 +0300

    move comments

--- internal/models/card.go
+++ internal/models/card.go
@@ -1,21 +1,12 @@
 package models
 
-type CreditCard struct {
-	// CardNumber is full credit card number
-	CardNumber string `json:"cardNumber"`
-	// ExpirationMonth is two-digit expiration month of the card.
-	ExpirationMonth string `json:"expirationMonth"`
-	// ExpirationYear is four-digit expiration year of the card
-	ExpirationYear string `json:"expirationYear"`
-	// CardHolderName is name of the cardholder as it appears on the card
-	CardHolderName string `json:"cardHolderName"`
-	// Cvv is Card Verification Value (CVV) or Card Security Code (CSC).
-	// It is typically only used during transaction processing.
-	Cvv string `json:"cvv"`
-	// BillingAddress an object containing details of the cardholder's billing address
-	BillingAddress string `json:"billingAddress"`
-	// CardType is type of credit card (e.g., Visa, Mastercard, American Express)
-	CardType string `json:"cardType"`
-	// IssuingBank name of the bank that issued the credit card
-	IssuingBank string `json:"issuingBank"`
+type BankCard struct {
+	CardNumber      string `json:"cardNumber"`      // CardNumber is full bank card number
+	ExpirationMonth string `json:"expirationMonth"` // ExpirationMonth is two-digit expiration month of the card.
+	ExpirationYear  string `json:"expirationYear"`  // ExpirationYear is four-digit expiration year of the card
+	CardHolderName  string `json:"cardHolderName"`  // CardHolderName is name of the cardholder as it appears on the card
+	Cvv             string `json:"cvv"`             // Cvv is Card Verification Value (CVV) or Card Security Code (CSC).
+	BillingAddress  string `json:"billingAddress"`  // BillingAddress an object containing details of the cardholder's billing address
+	CardType        string `json:"cardType"`        // CardType is type of bank card (e.g., Visa, Mastercard, American Express)
+	IssuingBank     string `json:"issuingBank"`     // IssuingBank name of the bank that issued the bank card
 }
